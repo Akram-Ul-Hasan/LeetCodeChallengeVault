8b45062dbb43cfbbed216136a1733a6786283629
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2024-07-01 12:03:59 +0600

    Longest Palindrome

---
+++ LeetCodeChallengeVault/Swift/Longest_Palindrome.swift
@@ -0,0 +1,25 @@
+
+import Foundation
+class Longest_Palindrome{
+    func longestPalindrome(_ s: String) -> Int {
+        var sDict = [Character: Int]()
+
+        for char in s {
+            sDict[char, default: 0] += 1
+        }
+
+        var result = 0
+        var isOddAdded = false
+        for (_, count) in sDict {
+            if count%2 == 0 {
+                result += count
+            } else if isOddAdded == false {
+                result += count
+                isOddAdded = true
+            } else {
+                result += count - 1
+            }
+        }
+        return result
+    }
+}
